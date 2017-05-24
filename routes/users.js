bec513a008488686b7d4af518df77ea3252063d5
farnaz <farnaz.rashidi@gmail.com>
2017-05-24 16:23:19 -0700

    Modified the skeleton of the project

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,7 @@ const router  = express.Router();
 module.exports = (knex) => {
 
   router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
+
   });
 
   return router;
