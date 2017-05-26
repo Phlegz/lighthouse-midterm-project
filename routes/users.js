93d5ee678fb6bddd149185de8ca0979f44a10f0a
Tin Trung Dang <tindang1710@gmail.com>
2017-05-26 11:35:04 -0700

    created .env file for sensitive information

--- routes/users.js
+++ routes/users.js
@@ -3,19 +3,25 @@
 // Step1: create the order in the database with all the order fields firstName -> totalPaid
 // Step2: get the id from the new created order
 // Step3: for each element in lineItems create new line_item in database with dishId and orderId and quantity
+require('dotenv').config();
+
 const express = require('express');
 const router  = express.Router();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 module.exports = (knex) => {
 
   // Home page
   router.get("/", (req, res) => {
-    res.render("index");
+
   });
 
   // Checkout
   router.post("/", (req, res) => {
-    order = {
+    let order = {
       first_name: req.body.first_name,
       last_name: req.body.last_name,
       email: req.body.email,
@@ -34,6 +40,16 @@ module.exports = (knex) => {
     res.redirect("/order_id")
   });
 
+  router.post("/call", (req, res) => {
+    client.calls.create({
+      url: "https://demo.twilio.com/welcome/voice/",
+      to: "+16046556558",
+      from: "+14387937553"
+    }, function(err, call) {
+      process.stdout.write(call.sid);
+    });
+  })
+
   // restaurant page
   router.get("/restaurant/:restaurant_id/order_id", (req, res) => {
     if (order.order_id != req.params.order_id) {
@@ -43,5 +59,18 @@ module.exports = (knex) => {
     res.render("order_confirmation")
   });
 
+  router.post("/message", (req, res) => {
+    client.messages.create({
+      body: "Your order is ready in 10 minutes?",
+      to: "+16046556558",
+      from: "+14387937553"
+    }, function(err, message) {
+      process.stdout.write(message.sid);
+    });
+  })
   return router;
 }
+
+// use session to keep track of users that are not logged in when they order food.
+// if(!req.session.user_id) {
+  // req.session.user_id = uuidV4
--- server.js
+++ server.js
@@ -18,32 +18,12 @@ const knexLogger  = require('knex-logger');
 const uuidV4 = require ("uuid/v4");
 uuidV4();
 console.log(uuidV4());
-// TODO : uuid middleware
-let order = {
-  first_name: "Tin",
-  last_name: "Dang",
-  email: "tindang1710@gmail.com",
-  phone: "604-655-6558",
-  street: "4231 Beatrice",
-  city: "Vancouver",
-  region: "BC",
-  postal_code: "V5N 4H9"
-  payment_method: "cash",
-  total_paid_in_cents: "83",
-  line_items: [{
-    dish_id: "22",
-    quantity: "1"
-  }],
-  estimated_completion: "45 mins"
-}
-
 // const keyPublishable = process.env.PUBLISHABLE_KEY;
 // const keySecret = process.env.SECRET_KEY;
 // const stripe = require("stripe")(keySecret);
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -66,6 +46,9 @@ app.use(express.static("public"));
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
 
+app.get("/", (req, res) => {
+  res.render("index");
+})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
