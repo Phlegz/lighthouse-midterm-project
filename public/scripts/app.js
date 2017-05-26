fbb05699cc310d2275cd1d178ef3a06db601a81c
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-26 15:57:04 -0700

    fixed glitch with - button not removing from order summary when qty is 0

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,22 @@
+//added function to global scope
+function renderOrder() {
+  function makeLineItemHTML(item) {
+    var html = `
+        <div>${escape(item.qty)} ${escape(item.name)} ${escape(item.price)}
+        <img class= "plus" src="/images/plus.png" data-id="${item.id}" height="10px"><img class= "minus" src="/images/minus.png" data-id="${item.id}" height="10px">
+        </div>
+    `;
+    return html;
+  }
+  order = JSON.parse(localStorage.order);
+  var $orderContainer = $('#order-container');
+  $orderContainer.empty();
+  Object.keys(order).forEach(function(key) {
+    var item = order[key];
+    $orderContainer.prepend(makeLineItemHTML(item));
+  });
+}
+
 $(function() {
 
   function makeOrderSummaryHTML(order) {
@@ -14,23 +33,25 @@ $(function() {
   }
 
   function orderIncrement(data, amount) {
-    console.log("AMOUNT", data);
     var id = data.id;
     var name = data.name;
     var price = data.price;
     if (order[id]) {
       order[id].qty += amount;
+      if (order[id].qty < 1 ) {
+        delete order[id];
+      } 
     } else {
       order[id] = {
-        id,
-        name,
-        price,
-        qty: 1
-      };
+      id,
+      name,
+      price,
+      qty: 1
+    };
     }
     localStorage.order = JSON.stringify(order);
     renderOrder();
-  } 
+  }
 
   $('.menu').on('click', '.add-to-cart', function(event) {
     event.preventDefault();
@@ -38,35 +59,15 @@ $(function() {
     
   });
   
-  $('body').on('click','.plus', function(event) {
+  $('body').on('click', '.plus', function(event) {
     event.preventDefault();
     orderIncrement(this.dataset, 1);
   });
 
-  $('body').on('click','.minus', function(event) {
+  $('body').on('click', '.minus', function(event) {
     event.preventDefault();
     orderIncrement(this.dataset, -1);
   });
 
   
 });
-
-//added function to global scope 
-function renderOrder() {
-    function makeLineItemHTML(item) {
-      var html = `
-          <div>${escape(item.qty)} ${escape(item.name)} ${escape(item.price)}
-          <img class= "plus" src="/images/plus.png" data-id="${item.id}" height="10px"><img class= "minus" src="/images/minus.png" data-id="${item.id}" height="10px">
-          </div>
-      `;
-      return html;
-    }
-    order = JSON.parse(localStorage.order);
-    var $orderContainer = $('#order-container');
-    $orderContainer.empty();
-    Object.keys(order).forEach(function(key) {
-      var item = order[key];
-      $orderContainer.prepend(makeLineItemHTML(item));
-    });
-  }
-
--- public/scripts/userform.js
+++ public/scripts/userform.js
@@ -1,21 +1,21 @@
 $(function() {
-    $(".userdetails").hide();
-    $(".edit-order").hide();
+  $(".userdetails").hide();
+  $(".edit-order").hide();
 
-    //the user form pops out and the edit button appears and the menu hides
-    $(".continue-button").click(function() {
+  //the user form pops out and the edit button appears and the menu hides
+  $(".continue-button").click(function() {
     $(".edit-order").show();
     $(".userdetails").show();
     $(".menu-container").hide();
     $(".continue-button").hide();
     $(".plus").hide();
     $(".minus").hide();
-  
-    // set error message if there are no items in the cart
 
-    });
-    //if the edit button is pressed the menu comes back and the form hides
-    $(".edit-order").click(function(){
+  // set error message if there are no items in the cart
+
+  });
+  //if the edit button is pressed the menu comes back and the form hides
+  $(".edit-order").click(function(){
     $(".userdetails").hide();
     $(".edit-order").hide();
     $(".menu-container").show();
@@ -23,8 +23,8 @@ $(function() {
     $(".plus").show();
     $(".minus").show();
 
-    })
-  
+  });
+
   renderOrder();
 
 });
