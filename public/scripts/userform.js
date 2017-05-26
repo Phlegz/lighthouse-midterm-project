d7cabfc24ffec3e40c02b18bdaabc3fc65b52f38
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-26 13:59:38 -0700

    Fixed bug where orders and + - were not populating when edit was clicked

--- public/scripts/userform.js
+++ public/scripts/userform.js
@@ -1,23 +1,30 @@
 $(function() {
-
-$(".userdetails").hide();
-$(".edit-order").hide();
-
-//the user form pops out and the edit button appears and the menu hides
-$(".continue-button").click(function() {
-    $(".edit-order").show();
-    $(".userdetails").show();
-    $(".menu").hide();
-    $(".continue-button").hide();
- // set error message if there are no items in the cart
-
-});
-//if the edit button is pressed the menu comes back and the form hides
-$(".edit-order").click(function(){
     $(".userdetails").hide();
     $(".edit-order").hide();
-    $(".menu").show();
+
+    //the user form pops out and the edit button appears and the menu hides
+    $(".continue-button").click(function() {
+    $(".edit-order").show();
+    $(".userdetails").show();
+    $(".menu-container").hide();
+    $(".continue-button").hide();
+    $(".plus").hide();
+    $(".minus").hide();
+  
+    // set error message if there are no items in the cart
+
+    });
+    //if the edit button is pressed the menu comes back and the form hides
+    $(".edit-order").click(function(){
+    $(".userdetails").hide();
+    $(".edit-order").hide();
+    $(".menu-container").show();
     $(".continue-button").show();
-    
-})
+    $(".plus").show();
+    $(".minus").show();
+
+    })
+  
+  renderOrder();
+
 });
