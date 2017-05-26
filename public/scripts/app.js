fbb13db2d7d24fd53c97de070180e4cccad1b3ce
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-26 15:38:08 -0700

    added subtract feature in cart

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,43 +13,51 @@ $(function() {
     order = JSON.parse(localStorage.order);
   }
 
-  //when any 'add to cart' button is pressed it passes a JSON with the dish info 
-  //to the renderOrder function
-  //if the button is pressed more than once, it updates the quanity 
-
-function orderIncrement(amount) {
-  console.log("AMOUNT", amount);
-  var id = this.dataset.id;
-  var name = this.dataset.name;
-  var price = this.dataset.price;
+  function orderIncrement(data, amount) {
+    console.log("AMOUNT", data);
+    var id = data.id;
+    var name = data.name;
+    var price = data.price;
     if (order[id]) {
-      order[id].qty + amount;
+      order[id].qty += amount;
     } else {
       order[id] = {
+        id,
         name,
         price,
         qty: 1
       };
     }
     localStorage.order = JSON.stringify(order);
-    renderOrder(order);
-} 
+    renderOrder();
+  } 
 
   $('.menu').on('click', '.add-to-cart', function(event) {
-    orderIncrement(1);
+    event.preventDefault();
+    orderIncrement(this.dataset, 1);
+    
+  });
+  
+  $('body').on('click','.plus', function(event) {
+    event.preventDefault();
+    orderIncrement(this.dataset, 1);
   });
 
+  $('body').on('click','.minus', function(event) {
+    event.preventDefault();
+    orderIncrement(this.dataset, -1);
+  });
+
+  
 });
-
-
 
 //added function to global scope 
 function renderOrder() {
     function makeLineItemHTML(item) {
       var html = `
-          <ul>${escape(item.qty)} ${escape(item.name)} ${escape(item.price)}
-          <img class= "plus" src="/images/plus.png" height="10px"><img class= "minus" src="/images/minus.png" height="10px">
-          </ul>
+          <div>${escape(item.qty)} ${escape(item.name)} ${escape(item.price)}
+          <img class= "plus" src="/images/plus.png" data-id="${item.id}" height="10px"><img class= "minus" src="/images/minus.png" data-id="${item.id}" height="10px">
+          </div>
       `;
       return html;
     }
