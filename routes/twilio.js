f905e54aae3dd95d53431f0489d63c675db18172
Tin Trung Dang <tindang1710@gmail.com>
2017-05-27 22:10:29 -0700

    implement twilio routes that allows restaurant to response how long they will take to prepare the food through phone

--- routes/twilio.js
+++ routes/twilio.js
@@ -11,34 +11,39 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 module.exports = () => {
-  router.post("/call/twiml" , (req, res) => {
+  // route to handle twilio post request
+  router.post("/call/twiml", (req, res) => {
     res.render("twiml");
-  });
-
-  // router.post("/call", (req, res) => {
+  })
+  // route to handle post request when customer place an order and inform Twilio to call the restaurant
+  router.post("/call", (req, res) => {
     client.calls.create({
-      url: "https://2a555f13.ngrok.io/twilio/call/twiml",
+      url: "https://614c7384.ngrok.io/order/call/twiml",
       to: "+16046556558",
       from: "+14387937553"
     }, function(err, call) {
-      process.stdout.write(call.sid);
-    });
-  // })
-
-
-
+        if(err){
+          console.log(err);
+          response.status(500).send(err);
+          return;
+        }
+      });
+    res.redirect("orderconfirmation").end()
+  });
 
 
   router.post("/message", (req, res) => {
-    let customerNum = "+16046556558";
+    console.log(req.body.Digits);
     client.messages.create({
-      body: "Your order is ready in 10 minutes",
-      to: customerNum,
-      from: twilioNum
+      body: `Your order is ready in ${req.body.Digits} minutes`,
+      to: "+16046556558",
+      from: "+14387937553"
     }, function(err, message) {
-      process.stdout.write(message.sid);
+      if(err)
+      console.log(err);
     });
-    res.send("check your message");
+    res.setHeader("Content-Type", "text/xml")
+    res.send(`<?xml version="1.0" encoding="UTF-8"?><Response><Say>thank you, have a good day</Say></Response>`);
   })
   return router;
 }
