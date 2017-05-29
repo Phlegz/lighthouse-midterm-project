64f906459325dd328174534e54cae1b2df6b7c02
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-29 12:53:00 -0700

    eslint clean up

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,56 +10,55 @@ function calculateTotalOrder(order) {
     subTotal += item.price * item.qty;
     tax = subTotal * taxRates;
     total = Math.round(subTotal + tax);
-  })
-  console.log(total);
+  });
 }
 
 $(function() {
 
-function renderOrder() {
-  function makeLineItemHTML(item) {
-    var name = decodeURIComponent(item.name);
-    var html = `
-        <div class="dish" >${escape(item.qty)} ${name} ${escape(item.price/100)}
-        <img class= "plus" src="/images/plus.png" data-id="${item.id}" height="10px">  <img class= "minus" src="/images/minus.png" data-id="${item.id}" height="10px">
-        </div>
-    `;
-    return html;
+  function renderOrder() {
+    function makeLineItemHTML(item) {
+      var name = decodeURIComponent(item.name);
+      var html = `
+          <div class="dish"> ${escape(item.qty)} ${name} ${escape(item.price / 100)}
+          <img class="plus" src="/images/plus.png" data-id="${item.id}" height="10px">  <img class="minus" src="/images/minus.png" data-id="${item.id}" height="10px">
+          </div>
+      `;
+      return html;
+    }
+
+    //loops through the order and calcultes subtotal, tax, and total
+    order = JSON.parse(localStorage.order);
+    var $orderContainer = $("#order-container");
+    $orderContainer.empty();
+    var subTotal = 0;
+    var taxRates = 0.07;
+    var tax = 0;
+    var total = 0;
+    Object.keys(order).forEach(function(key) {
+      var item = order[key];
+      $orderContainer.prepend(makeLineItemHTML(item));
+      subTotal += item.price * item.qty;
+      tax = subTotal * taxRates;
+      total = subTotal + tax;
+    });
+
+    $("#totals .subtotal span").text((subTotal / 100).toFixed(2));
+    $("#totals .tax span").text((tax / 100).toFixed(2));
+    $("#totals .total span").text((total / 100).toFixed(2));
   }
 
-  //loops through the order and calcultes subtotal, tax, and total
-  order = JSON.parse(localStorage.order);
-  var $orderContainer = $('#order-container');
-  $orderContainer.empty();
-  var subTotal = 0;
-  var taxRates = 0.07;
-  var tax = 0;
-  var total = 0;
-  Object.keys(order).forEach(function(key) {
-    var item = order[key];
-    $orderContainer.prepend(makeLineItemHTML(item));
-    subTotal += item.price * item.qty;
-    tax = subTotal * taxRates;
-    total = subTotal + tax;
-  });
-
-  $('#totals .subtotal span').text((subTotal/100).toFixed(2));
-  $('#totals .tax span').text((tax/100).toFixed(2));
-  $('#totals .total span').text((total/100).toFixed(2));
-}
-
   function submitForm() {
-    const $form = $('form');
-    $form.on('submit', (e) => {
-      var total= calculateTotalOrder(order);
-      var newOrder = Object.assign(order,{total:total})
-      $('<input>').attr({
-        type:"hidden",
-        name:'order',
+    const $form = $("form");
+    $form.on("submit", (e) => {
+      var total = calculateTotalOrder(order);
+      var newOrder = Object.assign(order, {total: total});
+      $("<input>").attr({
+        type: "hidden",
+        name: "order",
         value: JSON.stringify(newOrder)
-      }).appendTo($form)
+      }).appendTo($form);
       localStorage.clear();
-    })
+    });
   }
 
   submitForm();
@@ -84,27 +83,27 @@ function renderOrder() {
 
     } else {
       order[id] = {
-      id,
-      name,
-      price,
-      qty: 1
-    };
+        id,
+        name,
+        price,
+        qty: 1
+      };
     }
     localStorage.order = JSON.stringify(order);
     renderOrder();
   }
 
-  $('.menu').on('click', '.add-to-cart', function(event) {
+  $(".menu").on("click", ".add-to-cart", function(event) {
     event.preventDefault();
     orderIncrement(this.dataset, 1);
   });
 
-  $('body').on('click', '.plus', function(event) {
+  $("body").on("click", ".plus", function(event) {
     event.preventDefault();
     orderIncrement(this.dataset, 1);
   });
 
-  $('body').on('click', '.minus', function(event) {
+  $("body").on("click", ".minus", function(event) {
     event.preventDefault();
     orderIncrement(this.dataset, -1);
   });
@@ -112,14 +111,14 @@ function renderOrder() {
 
 
   $(document).scroll(function() {
-    var y = $(document).scrollTop(), //get page y value
-        header = $(".sidenav"); // your div id
-    if(y >= 600)  {
-        header.css({position: "fixed", "top" : "0", "left" : "0", "padding-top": ".55em"});
+    var y = $(document).scrollTop();
+    header = $(".sidenav");
+    if (y >= 600)  {
+      header.css({position: "fixed", "top": "0", "left": "0", "padding-top": ".55em"});
     } else {
-        header.css("position", "static");
+      header.css("position", "static");
     }
-});
+  });
 
 
 });
